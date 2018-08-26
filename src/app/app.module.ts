7d6ba7f8368ee2c111e7db5226c41671739ebc1f
Michael <hpds.life@gmail.com>
2018-08-26 09:54:18 -0600

    v0.0.4 TaskComponent added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { TasksComponent } from './tasks/tasks.component';
 import { AddTaskComponent } from './tasks/add-task/add-task.component';
 import { FormsModule } from '@angular/forms';
 import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskComponent } from './tasks/task-list/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
     AddTaskComponent,
-    TaskListComponent
+    TaskListComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
